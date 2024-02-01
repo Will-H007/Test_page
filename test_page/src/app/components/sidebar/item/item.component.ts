be65ad729329bd93b1a95d033f282600e2e9a3f9
Will Ho <wkayi076@gmail.com>
2024-02-01 18:55:54 +0800

    fixed ngFor

--- test_page/src/app/components/sidebar/item/item.component.ts
+++ test_page/src/app/components/sidebar/item/item.component.ts
@@ -1,11 +1,13 @@
 // item.component.ts
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'sidebar-item',
   standalone:true,
   templateUrl: './item.component.html',
-  styleUrls: ['./item.component.css']
+  styleUrls: ['./item.component.css'],
+  imports: [CommonModule],
 })
 export class ItemComponent {
 
--- test_page/src/app/components/sidebar/sidebar.component.ts
+++ test_page/src/app/components/sidebar/sidebar.component.ts
@@ -1,19 +1,34 @@
 // sidebar.component.ts
 import { Component } from '@angular/core';
 import { ItemComponent } from './item/item.component';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-sidebar',
   standalone:true,
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
-  imports: [ItemComponent],
+  imports: [ItemComponent, CommonModule],
 })
 export class SidebarComponent {
   menuItems = [
     { title: 'Dashboard', count: 16 },
-    { title: 'Orders', count: 0 }, // Replace 'your-orders-svg' with the actual SVG content
-    { title: 'Products', count: 0 }, // Replace 'your-products-svg' with the actual SVG content
-    // Add more items as needed
+    { title: 'Orders', count: 0 },
+    { title: 'Products', count: 0 },
+    { title: 'Categories', count: 8 },
+    { title: 'Customers', count: 22 },
+    { title: 'Reports', count: 5 },
+    { title: 'Coupons', count: 10 },
+    { title: 'Inbox', count: 3 },
   ];
+
+  otherInformation = [
+    { title: 'Knowledge Base', count: 10 },
+    { title: 'Product Updates', count: 3 },
+  ]
+
+  settingItems = [
+    { title: 'Personal Settings', count: 10 },
+    { title: 'Global Settings', count: 5 },
+  ]
 
 }
