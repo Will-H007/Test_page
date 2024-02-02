298def86e60414b261765a2d7a7b8cdac2faff14
Will Ho <wkayi076@gmail.com>
2024-02-02 11:33:43 +0800

    added fields

--- test_page/src/app/components/content/settings/profile/profile_section/profile_section.component.ts
+++ test_page/src/app/components/content/settings/profile/profile_section/profile_section.component.ts
@@ -2,23 +2,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TextFieldComponent } from '../../../../fields/textfield/textfield.component';
+import { UploadFieldComponent } from '../../../../fields/uploadfield/uploadfield.component';
 @Component({
   selector: 'profile_section',
   standalone: true,
   templateUrl: './profile_section.component.html',
   styleUrls: ['./profile_section.component.css'],
-  imports: [ CommonModule, TextFieldComponent],
+  imports: [ CommonModule, TextFieldComponent, UploadFieldComponent],
 })
 export class ProfileSectionComponent {
   textfields = [
     { label:"testing1" },
     { label:"testing2" },
     { label:"testing3" },
-    { label:"testing1" },
-    { label:"testing2" },
-    { label:"testing3" },
-   
-   
+    { label:"testing4" },
   ];
+
+  uploadlabel = "Profile Image";
 
 }
